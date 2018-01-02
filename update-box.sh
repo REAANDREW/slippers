9f23228d0606483407a7dd77769f6442bb4e6f50
Andrew Rea <email@andrewrea.co.uk>
2018-01-02 23:34:17 +0000

    tidying up

--- update-box.sh
+++
@@ -1,7 +0,0 @@
-box_name="slippers-golang-ubuntu-16.10"
-
-vagrant box add --force $box_name builds/VirtualBox-ubuntu-server-16.10.box
-mkdir -p workbench
-pushd workbench
-vagrant init $box_name
-popd
